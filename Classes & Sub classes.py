92e7969d5d149506544183424022a42ce4a15f21
Martin Pruus <50612280268@tptlive.ee>
2024-11-06 17:37:15 +0200

    Updated py file

--- Classes & Sub classes.py
+++ Classes & Sub classes.py
@@ -29,6 +29,9 @@ Motorcycle1 = Motorcycle("Motorcycle")
 Bicycle1 = Bicycle("Bicycle")
 Car1 = Car("BMW E30 325i")
 
+print()
+print("Car")
+
 Car1.CarType()
 
 
@@ -57,6 +60,9 @@ class Son(Person):
 father = Father("Andrus", 45, "Mechanic")
 mother = Mother("Anna", 43, "Libearian")
 son = Son("Rauno", 15, "Quitar player")
+
+print()
+print("Person")
 
 father.AllDetails()
 mother.AllDetails()
@@ -101,6 +107,10 @@ class Triangle(Shape):
         perimeter = self.dim1 + self.dim2 + side3
         print("Perimeter of triangle is:", perimeter)
 
+
+print()
+print("Shapes")
+
 square = Square(5, 0)  
 
 circle = Circle(3, 0)
@@ -114,3 +124,162 @@ rectangle.rectangle_perimeter()
 triangle = Triangle(4, 5) 
 triangle.triangle_area()
 triangle.triangle_perimeter(6)
+
+
+# Calculation class
+class calculation:
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+class calculation1(calculation):
+    def calculation1(self):
+        result = self.a + self.b + self.c + self.d
+        print(f"{self.a} + {self.b} + {self.c} + {self.d} = ", result)
+class calculation2(calculation):
+    def calculation2(self):
+        result = self.a - self.b + self.c - self.d
+        print(f"{self.a} - {self.b} + {self.c} - {self.d} = ", result)
+class calculation3(calculation):
+    def calculation3(self):
+        result = self.a * self.b + self.c / self.d
+        print(f"{self.a} * {self.b} + {self.c} / {self.d} = ", result)
+class calculation4(calculation):
+    def calculation4(self):
+        result = self.a / self.b + self.c * self.d
+        print(f"{self.a} / {self.b} + {self.c} * {self.d} = ", result)
+
+Calculation1 = calculation1(1, 2, 3, 4)
+Calculation2 = calculation2(1, 2, 3, 4)
+Calculation3 = calculation3(1, 2, 3, 4)
+Calculation4 = calculation4(1, 2, 3, 4)
+
+print()
+print("Calculation")
+
+Calculation1.calculation1()
+Calculation2.calculation2()
+Calculation3.calculation3()
+Calculation4.calculation4()
+
+
+# Animal class
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    def eat(self):
+        print(f"{self.name} is eating.")
+    def sleep(self):
+        print(f"{self.name} is sleeping.")
+
+class Prey(Animal):
+    def flee(self):
+        print(f"{self.name} is fleeing.")
+class Predator(Animal):
+    def hunt(self):
+        print(f"{self.name} is hunting.")
+class Rabbit(Prey):
+    pass
+class Hawk(Predator):
+    pass
+class Fish(Prey, Predator):
+    pass
+
+fish = Fish("Kala")
+rabbit = Rabbit("Jänes")
+hawk = Hawk("Kotkas")
+
+print()
+print("Animal")
+
+fish.hunt()
+fish.flee()
+fish.eat()
+fish.sleep()
+hawk.hunt()
+hawk.eat()
+hawk.sleep()
+rabbit.flee()
+rabbit.eat()
+rabbit.sleep()
+
+# Calculation class2 
+class calculation:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def add(self):
+        sum = self.a + self.b
+        print("Sum is ", sum)
+class calculation1(calculation):
+    def __init__(self, a, b, c, d):
+        super().__init__(a, b)
+        self.c = c
+        self.d = d
+    def add1(self):
+        sum = self.a + self.b + self.c +self.d
+        print(f"{self.a} + {self.b} + {self.c} + {self.d} = ", sum) 
+class calculation2(calculation1):
+    def __init__(self, a, b, c, d, e):
+        super().__init__(a, b, c ,d)
+        self.e = e
+    def add2(self):
+        sum = self.a +self.b + self.c + self.d + self.e
+        print(f"{self.a} + {self.b} + {self.c} + {self.d} + {self.e} = ", sum)
+class calculation3(calculation2):
+    def __init__(self, a, b, c, d, e, f):
+        super().__init__(a, b, c, d, e)
+        self.f = f
+    def add3(self):
+        sum = self.a +self.b + self.c + self.d + self.e + self.f
+        print(f"{self.a} + {self.b} + {self.c} + {self.d} + {self.e} + {self.f} = ", sum)
+
+cal1 = calculation1(1, 2, 3, 4)
+cal2 = calculation2(1, 2, 3, 4, 5)
+cal3 = calculation3(1, 2, 3, 4, 5, 6)
+
+print()
+print("Calculation2")
+
+cal1.add1()
+cal2.add2()
+cal3.add3()
+
+
+# Human class
+class Human:
+    def __init__(self, name, age, height):
+        self.name = name
+        self.age = age
+        self.height = height
+class Einstein(Human):
+    def __init__(self, name, age, height, iq):
+        super().__init__(name, age, height)
+        self.iq = iq
+    def EinsteinData(self):
+        print(f"{self.name} {self.age} {self.height} {self.iq}")
+class Alex(Human):
+    def __init__(self, name, age, height, is_smart):
+        super().__init__(name, age, height)
+        self.is_smart = is_smart
+    def AlexData(self):
+        print(f"{self.name} {self.age} {self.height} {self.is_smart}")
+class Newton(Human):
+    def __init__(self, name, age, height, formula):
+        super().__init__(name, age, height)
+        self.formula = formula
+    def NewtonData(self):
+        print(f"{self.name} {self.age} {self.height} {self.formula}")
+
+einstein = Einstein("Albert", 79, 170, 145)
+alex = Alex("Alex", 17, 178, "wdadasd")
+newton = Newton("Newton", 80, 170, "formula")
+
+print()
+print("Human")
+
+einstein.EinsteinData()
+alex.AlexData()
+newton.NewtonData()
