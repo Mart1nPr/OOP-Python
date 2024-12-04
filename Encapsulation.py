abf60656867a3b83910c638f12495675e042c4c1
Martin Pruus <martin.pruus@tptlive.ee>
2024-12-04 16:49:01 +0200

    Updated Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -7,16 +7,34 @@ class Cars:
         self._speed = speed # _ _ Warns, but you can change
     
     def details(self):
-        print(f"Brand name {self.brand_name} \n color: {self.__color} \n speed: {self._speed}")
+        print(f"Brand name: {self.brand_name} \n color: {self.__color} \n speed: {self._speed}")
 
 toyota = Cars("Toyota", "Red", 250)
 
 # Old informatiom
-print("Old information:")
+print("Old information")
 toyota.details()
 
 # Changed information 
-print("New information:")
+print("New information")
 toyota.color = "Blue"
 toyota.speed = 300
 toyota.details()
+
+print() # Space
+
+# Exercise
+class Person:
+    def __init__(self, name, age):
+        self.__name = name # Can't change the name
+        self.__age = age # Can't change the age
+    
+    def personalData(self):
+        print(f"Name: {self.__name} \n Age: {self.__age}")
+
+David = Person("David", 69)
+print("Old information")
+David.personalData()
+David.__name = "John" # Name change
+David.__age = 420 # Age change
+David.personalData()
