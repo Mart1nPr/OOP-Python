44e9b1ef75c1fb85e7bf06d1bafbbbda9a2181f4
Martin Pruus <martin.pruus@tptlive.ee>
2024-12-11 17:01:43 +0200

    Updated Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -75,3 +75,68 @@ class Rice_Price:
 rice = Rice_Price("ABC", 5) # Company name & price
 rice.set_final_price(20) # Procentage of discount %
 print("Discount price: ",rice.get_final_price())
+
+print() # Space
+
+# ----Person class----
+class Human:
+    def __init__(self, name, iq):
+        self.__name = name
+        self.__iq = iq
+    def set_iq(self, new_iq):
+        self.new_iq = new_iq
+    def get_iq(self):
+        return self.__iq
+    def details(self):
+        print(f"{self.__name} has an iq of {self.__iq}")
+
+person = Human("David", 70)
+person.details()
+person.set_iq = 80 # Can't change
+person.details()   
+
+print() # Space
+
+# ----Tree class----
+class Tree:
+    def __init__(self, name, height):
+        self.__name = name
+        self.__height = height
+    def set_height(self, new_height):
+        self.new_height = new_height
+    def get_height(self):
+        return self.__height
+    def details(self):
+        print(f"{self.__name} is {self.__height}m tall")
+
+tree = Tree("Oak", 400)
+tree.details()
+tree.set_height = 15
+tree.get_height()
+
+print() # Space
+
+# ----Employee class----
+class Employee:
+    def __init__(self, name, position, salery):
+        self.name = name
+        self.position = position
+        self.salery = salery
+    def get_status(self):
+        print(f"Current status: {self.name} has a position of {self.position} and has a salery of {self.salery}€")
+    def update_salery(self, new_salery):
+        self.new_salery = new_salery
+        if self.new_salery > self.__salery:
+            self.__salery = new_salery
+        else:
+            print("New salery must be higher than old salery")
+    def promote(self, new_position, salery_increase):
+        self.new_position = new_position
+        self.salery_increase = salery_increase
+        print(f"You promoted {self.__position} and new salery {self.salery}")
+
+Worker = Employee("John", "Junior-SoftwareDeveloper", 3000)
+Worker.get_status()
+Worker.update_salery(5000)
+Worker.promote("Senior-SoftwareDeveloper", 5000)
+Worker.get_status()
